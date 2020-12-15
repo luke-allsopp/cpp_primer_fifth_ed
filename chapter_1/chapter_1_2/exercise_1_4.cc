34bbcb65241e4e91345b12dbc7ad7cef9a3ce715
luke-allsopp <lukeallsopp93@gmail.com>
2020-12-15 11:55:47 +1100

    ex1_4

---
+++ chapter_1/chapter_1_2/exercise_1_4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main ()
+{
+    cout << "Enter two numbers:" << endl;
+    int v1 = 0, v2 = 0;
+
+    cin >> v1 >> v2;
+    cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2 << endl;
+
+    return 0;
+}
